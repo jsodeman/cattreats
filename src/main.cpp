01ea42e90302cbb2b2bed7a68f08f8e9c93e2ba1
Jeff Sodeman <jeff@thecodeexperts.com>
2020-07-11 19:14:41 -0400

    working with stepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <Stepper.h>
+
+#define STEPS 2048 //2038
+#define IN1   D1   // IN1 is connected to NodeMCU pin D1 (GPIO5)
+#define IN2   D2   // IN2 is connected to NodeMCU pin D2 (GPIO4)
+#define IN3   D3   // IN3 is connected to NodeMCU pin D3 (GPIO0)
+#define IN4   D4   // IN4 is connected to NodeMCU pin D4 (GPIO2)
+
+Stepper stepper(STEPS, IN4, IN2, IN3, IN1);
 
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(115200);
+
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(IN3, OUTPUT);
+	pinMode(IN4, OUTPUT);
+  	stepper.setSpeed(10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	stepper.step(1624);
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, LOW);
+	digitalWrite(IN3, LOW);
+	digitalWrite(IN4, LOW);
+	delay(5000);
 }
