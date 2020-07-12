aca666e86e17cfff06fbdd16448632a990babd98
Jeff Sodeman <jeff@thecodeexperts.com>
2020-07-11 22:26:44 -0400

    blocking issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <Stepper.h>
+#include <math.h>
 
-#define STEPS 2048 //2038
+#define STEPS 2076 //2038
 #define IN1   D1   // IN1 is connected to NodeMCU pin D1 (GPIO5)
 #define IN2   D2   // IN2 is connected to NodeMCU pin D2 (GPIO4)
 #define IN3   D3   // IN3 is connected to NodeMCU pin D3 (GPIO0)
@@ -9,21 +10,33 @@
 
 Stepper stepper(STEPS, IN4, IN2, IN3, IN1);
 
+void ICACHE_RAM_ATTR btn_full() {
+ 	Serial.println("One Turn");
+	stepper.step(STEPS);
+	
+}
+
+void ICACHE_RAM_ATTR btn_small() {
+ 	Serial.println("Small Turn");
+	stepper.step(1);
+}
+
 void setup() {
 	Serial.begin(115200);
 
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
-  	stepper.setSpeed(10);
+  	stepper.setSpeed(5);
+	pinMode(D6, INPUT_PULLUP);
+	pinMode(D7, INPUT_PULLUP);
+	attachInterrupt(D6, btn_full, RISING);
+	attachInterrupt(D7, btn_small, RISING);
 }
 
 void loop() {
-	stepper.step(1624);
-	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
-	delay(5000);
+	// stepper.step(1624);
+	// digitalWrite(IN1, LOW);
+	// digitalWrite(IN2, LOW);
+	// digitalWrite(IN3, LOW);
+	// digitalWrite(IN4, LOW);
+	// delay(5000);
+	yield();
 }
